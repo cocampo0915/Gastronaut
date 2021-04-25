490c5c87af79a29de98d37a3b5cc0689c15104f6
=Camille Ocampo <=cocampo0915@gmail.com>
2021-04-24 18:35:40 -0700

    wip: add modal view to show page for menu items

---
+++ public/script.js
@@ -0,0 +1,27 @@
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+$menuItems = $('#menu-items');
+$itemDetails = $('.modal-content');
+
+function displayItemDetails(e) {
+  const parent = e.target.parentElement;
+  const modal = parent.getElementsByClassName("modal")[0];
+  console.log(modal);
+  modal.style.display = "block";
+}
+
+function closeDetails(e) {
+  const parent = e.target.parentElement.parentElement.parentElement;
+  const modal = parent.getElementsByClassName("modal")[0];
+  console.log(modal);
+  modal.style.display = "none";
+}
+
+$menuItems.on('click', '.card', displayItemDetails);
+$itemDetails.on('click', '.close', closeDetails);
+
